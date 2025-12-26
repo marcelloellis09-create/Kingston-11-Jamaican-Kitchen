bdb5f68fef2e83392ea2693ffe86893354e11806
Marcello Ellis <marcelloellis09@gmail.com>
2025-12-26 03:13:46 -0500

    Update Kingston11Main.js
    Just ended up learning about pressing buttons for js. planning on upgrading the html to actually fit the needs of the page.

--- Kingston11Main.js
+++ Kingston11Main.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
+
     let orderButton = document.getElementById("confirmorder");
+    let OrderList = {
+        "Chicken": 0,
+        "Pork": 0
+    };
+    orderButton.addEventListener("click", function() {
+        console.log(OrderList);
+    })
+
 });
